d46c810c6bc0f2541d202c3ea3da5ae7676be34f
Tommaso Poletti <polettitommaso@gmail.com>
2020-03-27 00:16:44 +0100

    added netlify attr

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,7 +1,7 @@
 export default function Form() {
 	return (
 		<div class="">
-			<form name="contact" netlify>
+			<form name="contact" method="POST" data-netlify="true">
 				<p>
 					<label>Nome
 						<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="name" />
