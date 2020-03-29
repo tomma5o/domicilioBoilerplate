25c016067b005f31998e0f3f693b6857c2d3a205
Tommaso Poletti <polettitommaso@gmail.com>
2020-03-30 00:39:28 +0200

    minor fixes in form.js

--- src/routes/form.js
+++ src/routes/form.js
@@ -16,6 +16,11 @@ export default function Form() {
 					</label>
 				</p>
 				<p class="my-5">
+               <label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Email
+                  <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="mail" />
+               </label>
+            </p>
+				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Sito Web
 						<input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="site" />
 					</label>
@@ -26,7 +31,7 @@ export default function Form() {
 				</p>
 				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Note
-						<textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="email" />
+						<textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="mail" />
 					</label>
 				</p>
 				<p class="my-5">
