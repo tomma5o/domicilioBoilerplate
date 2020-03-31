137308d188bf97dd61cb5385acb3aa7dbb5076a6
Tommaso Poletti <polettitommaso@gmail.com>
2020-03-31 22:56:55 +0200

    filter categories by cvarisco

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ export default class App extends Component {
 					<nav class="flex justify-center md:justify-end items-center">
 						{
 							isHomepage
-								? <Link class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded" href="/form">Aggiungi un'attività</Link>
+								? <Link class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded" href="/form">➕ Aggiungi un'attività</Link>
 								: <Link class="m-5 text-blue-500 hover:text-blue-800" href="/">Ritorna alla ricerca</Link>
 						}
 					</nav>
