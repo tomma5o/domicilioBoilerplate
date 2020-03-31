398951b201a4f473f71a1a181b3ee281846ba38f
Tommaso Poletti <polettitommaso@gmail.com>
2020-03-31 21:51:51 +0200

    fix form && navigation

--- src/index.js
+++ src/index.js
@@ -74,10 +74,9 @@ export default class App extends Component {
 					<nav class="flex justify-center md:justify-end items-center">
 						{
 							isHomepage
-								? null
+								? <Link class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded" href="/form">Aggiungi un'attività</Link>
 								: <Link class="m-5 text-blue-500 hover:text-blue-800" href="/">Ritorna alla ricerca</Link>
 						}
-						<Link class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded" href="/form">Aggiungi un'attività</Link>
 					</nav>
 					<h1 class="font-sans text-4xl md:text-5xl lg:text-6xl pt-10 text-gray-800 text-center capitalize">
 						<span class="block sm:inline-block" role="img" aria-label="biker">
--- src/routes/form.js
+++ src/routes/form.js
@@ -31,7 +31,7 @@ export default function Form() {
 				</p>
 				<p class="my-5">
 					<label class="lock text-gray-800 ml-2 font-bold md:text-right mb-1 md:mb-0 pr-4">Note
-						<textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="mail" />
+						<textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" name="note" />
 					</label>
 				</p>
 				<p class="my-5">
