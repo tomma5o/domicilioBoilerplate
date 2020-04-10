17d3596a74e5589f27fe85b214d7f6d0536d092f
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-04-10 23:58:09 +0200

    Refactored sort util script

--- utils/sortJson.js
+++ utils/sortJson.js
@@ -1,24 +1,19 @@
 const json = require("../data/gist.json");
+const output = {};
 
-function compare(a, b) {
-   if (a.name < b.name) {
-      return -1;
-   }
-   if (a.name > b.name) {
-      return 1;
-   }
+const compare = (a, b) => {
+   if (a.name < b.name) return -1;
+   if (a.name > b.name) return 1;
    return 0;
 }
-
-const output = {};
 
 for (let name in json) {
    let current = json[name];
    output[name] = {
       icon: current.icon,
-      data: current.data.sort(compare)
-   }
+      data: current.data.sort(compare),
+   };
 }
 
-fs = require('fs');
+fs = require("fs");
 fs.writeFileSync("./data/sorted.json", JSON.stringify(output));
