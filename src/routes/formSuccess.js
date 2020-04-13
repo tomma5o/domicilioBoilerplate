4f2b398f230c93693687d02fb26d66c16a6234fc
verlok <andrea.verlicchi@gmail.com>
2020-04-14 00:24:59 +0200

    Updated form success message

--- src/routes/formSuccess.js
+++ src/routes/formSuccess.js
@@ -1,10 +1,10 @@
 export default function FormSuccess() {
 	return (
 		<div class="mt-10 text-center">
-			<p class="text-2xl">Grazie per aver aggiunto l'attività!</p>
+			<p class="text-2xl">Grazie per la segnalazione!</p>
 			<p class="mt-5">Abbiamo preso in carico la tua richiesta, normalmente la fase di approvazione richiede 24 ore.</p>
-			<p class="mt-5">Nel frattempo, perché non condividi quest'app con chi abita a {process.env.PREACT_APP_CITY}? Gli farà piacere!</p>
-			<p class="text-5xl" aria-label="thanks icons">Grazie 🙏</p>
+			<p class="mt-2">Nel frattempo, perché non condividi quest'app con chi abita a {process.env.PREACT_APP_CITY}? Gli farà piacere!</p>
+			<p class="mt-5 text-5xl" aria-label="thanks icon">🙏</p>
 		</div>
 	);
 }
