6bafadc049ff10f94f05824d152192a28afb6410
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-04-15 17:50:43 +0200

    Removed comments from sw

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,6 @@
-/* eslint-disable no-undef */
-
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 const isNav = (event) => event.request.mode === "navigate";
-
-/*
- * Listeners
- */
 
 self.addEventListener("activate", () => {
    caches
@@ -14,12 +8,7 @@ self.addEventListener("activate", () => {
       .then((cache) => cache.add(`${process.env.PREACT_APP_DATA_SOURCE}`));
 });
 
-/*
- * Hand-made plugin
- */
-
 const useLastCachedResponse = ({ cacheName, cachedResponse }) => {
-   // If there's already a match against the request URL, return it.
    if (cachedResponse) {
       return cachedResponse;
    }
@@ -30,14 +19,9 @@ const useLastCachedResponse = ({ cacheName, cachedResponse }) => {
       .then((keys) => caches.match(keys[keys.length - 1].url));
 };
 
-/**
- * Adding this before `precacheAndRoute` lets us handle all
- * the navigation requests even if they are in precache.
- */
 workbox.routing.registerRoute(
    ({ event }) => isNav(event),
    new workbox.strategies.NetworkFirst({
-      // this cache is plunged with every new service worker deploy so we dont need to care about purging the cache.
       cacheName: workbox.core.cacheNames.precache,
       networkTimeoutSeconds: 5,
       plugins: [
