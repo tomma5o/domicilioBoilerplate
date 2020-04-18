5fbd27ddc6c2b85041b216b24069b62580d486f1
Tommaso Poletti <polettitommaso@gmail.com>
2020-04-18 16:40:05 +0200

    fix plugin in preact.config.js

--- preact.config.js
+++ preact.config.js
@@ -1,13 +1,11 @@
 import envVars from 'preact-cli-plugin-env-vars';
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 
-function moveFileAssetsInRoot(config) {
-    config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }]) );
-}
-
 export default {
    plugins: [
       envVars,
-      moveFileAssetsInRoot,
    ],
+   webpack(config) {
+      config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }]) );
+   }
 }
