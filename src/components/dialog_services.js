5f5ced0acd34a4cf4fcfe22d6d1b67e639cb6903
Tommaso Poletti <polettitommaso@gmail.com>
2020-04-19 12:41:27 +0200

    🔗 Removed link for services

--- src/components/dialog_services.js
+++ src/components/dialog_services.js
@@ -3,7 +3,7 @@ import { Fragment } from "preact";
 // Utils
 import getBrandColor from '../utils/getBrandColor';
 
-export const D_Services = ({services, name}) => {
+export const D_Services = ({services}) => {
 	
 	return (
 		<Fragment>
@@ -11,21 +11,16 @@ export const D_Services = ({services, name}) => {
 			<div>
 				{services.map((service) => {
 					const colors = getBrandColor(service);
-					const encodedShopName = encodeURIComponent(name);
-					const encodedCity = encodeURIComponent(
-						process.env.PREACT_APP_CITY
-					);
 
 					return (
-						<a
-							href={`https://www.google.com/search?q=${encodedShopName}%20${encodedCity}%20${service}`}
+						<span
 							class="inline-block py-1 px-3 mb-2 mr-2 text-sm rounded-full font-semibold"
 							target="_blank"
 							rel="noopener noreferrer"
 							style={colors}
 						>
 							{service}
-						</a>
+						</span>
 					);
 				})}
 			</div>
