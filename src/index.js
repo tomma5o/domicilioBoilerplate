2929e0c68f6601c909744cdfab60ecc16cf6a324
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-04-28 15:00:57 +0200

    Temporary disable form

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import { h, Component, createContext } from 'preact';
-import { Router } from 'preact-router';
-import { Link } from 'preact-router/match';
+import { h, Component, createContext } from "preact";
+import { Router } from "preact-router";
+import { Link } from "preact-router/match";
 
-import './assets/styles/global.css';
+import "./assets/styles/global.css";
 
 // Routes
-import Home from './routes/home.js';
-import Form from './routes/form.js';
-import FormSuccess from './routes/formSuccess';
+import Home from "./routes/home.js";
+import Form from "./routes/form.js";
+import FormSuccess from "./routes/formSuccess";
 
 // Components
-import { Dialog } from './components/dialog.js';
-import { PWAPrompt } from './components/pwaPrompt';
+import { Dialog } from "./components/dialog.js";
+import { PWAPrompt } from "./components/pwaPrompt";
 
-export const Action = createContext({})
+export const Action = createContext({});
 
 export default class App extends Component {
-
 	state = {
 		results: {},
 		isHomepage: true,
 		isPopupOpen: false,
 		popupData: {},
-	}
+	};
 
-	handleRoute = e => {
+	handleRoute = (e) => {
 		this.currentUrl = e.url;
 		this.setState({ isHomepage: e.url.replace(/\?.*/g, "") === "/" });
 	};
@@ -34,23 +33,23 @@ export default class App extends Component {
 
 		this.setState({
 			popupData,
-			isPopupOpen: true
-		})
-	}
+			isPopupOpen: true,
+		});
+	};
 
-	closePopup = (e) => {	
+	closePopup = (e) => {
 		if (e.currentTarget === e.target) {
-			this.setState({ isPopupOpen: false })
+			this.setState({ isPopupOpen: false });
 		}
-	}
+	};
 
 	componentDidMount() {
 		fetch(`${process.env.PREACT_APP_DATA_SOURCE}`)
-			.then(r => r.json())
-			.then(json => {
+			.then((r) => r.json())
+			.then((json) => {
 				this.setState({
 					results: json,
-					resultBkp: json
+					resultBkp: json,
 				});
 			});
 	}
@@ -59,7 +58,10 @@ export default class App extends Component {
 		const { isPopupOpen } = this.state;
 
 		const root = document.documentElement;
-		root.style.setProperty('--popup-visible', isPopupOpen ? 'hidden' : 'initial')
+		root.style.setProperty(
+			"--popup-visible",
+			isPopupOpen ? "hidden" : "initial"
+		);
 	}
 
 	render(props, { isHomepage, results, popupData, isPopupOpen }) {
@@ -67,16 +69,25 @@ export default class App extends Component {
 			<Action.Provider value={{ setPopupNumbers: this.setPopupNumbers }}>
 				<div id="app" class="px-5 max-w-screen-md mx-auto">
 					<header>
-						<nav class="flex justify-center md:justify-end items-center">
+						<div class="m-5 text-center">
+							<p class="bg-gray-200 text-sm text-gray-500 px-2 py-1 rounded inline-block">
+								Segnalazione attività sospesa fino al 29/04
+							</p>
+						</div>
+						{/* <nav class="flex justify-center md:justify-end items-center">
 							{
 								isHomepage
 									? <Link class="m-5 bg-blue-500 inline-block text-white font-bold px-2 py-1 rounded" href="/form">➕ Aggiungi un'attività</Link>
 									: <Link class="m-5 text-blue-500" href="/">Ritorna alla ricerca</Link>
 							}
-						</nav>
+						</nav> */}
 						<h1 class="font-sans text-4xl md:text-5xl lg:text-6xl text-gray-800 text-center">
 							{`${process.env.PREACT_APP_CITY} a Domicilio`}
-							<span class="block sm:inline-block ml-2" role="img" aria-label="biker">
+							<span
+								class="block sm:inline-block ml-2"
+								role="img"
+								aria-label="biker"
+							>
 								🚴
 							</span>
 						</h1>
@@ -87,7 +98,11 @@ export default class App extends Component {
 						<FormSuccess path="/form/success" />
 					</Router>
 				</div>
-				<Dialog isOpen={isPopupOpen} closePopup={this.closePopup} {...popupData} />
+				<Dialog
+					isOpen={isPopupOpen}
+					closePopup={this.closePopup}
+					{...popupData}
+				/>
 				<PWAPrompt />
 			</Action.Provider>
 		);
